44b46083cfc3a7686a44ec46546039b69266f120
trungvuong55555 <trungvuong55555@gmail.com>
2023-06-11 20:58:19 +0700

    add solutions

---
+++ Script-1.sql
@@ -0,0 +1,119 @@
+-- cau 1
+
+create or replace function distance (x1 int, y1 int, x2 int, y2 int)
+returns float 
+language plpgsql
+as $$
+declare
+final_result float;
+begin
+	select sqrt(pow(x1-x2,2) + pow(y1-y2,2)) into final_result;
+	return final_result;	
+end;
+$$;
+
+select distance(1,2,5,3);
+
+-- cau 2
+
+SELECT EXTRACT(EPOCH FROM TIMESTAMP '2011-05-17 10:40:28') * 1000;
+
+-- cau 3
+
+create function count_appear (userId int, start_timestamp timestamp, end_timestamp timestamp)
+returns int
+language plpgsql
+as $$
+declare
+final_result int;
+start_miliseconds bigint;
+end_miliseconds bigint;
+begin
+	SELECT EXTRACT(EPOCH FROM  start_timestamp) * 1000 into start_miliseconds;
+	SELECT EXTRACT(EPOCH FROM  end_timestamp )* 1000 into end_miliseconds;
+	select count(*) from tracking t where t.user_id = userId and t.millisecond >=  start_miliseconds and t.millisecond < end_miliseconds into final_result;
+	return final_result;
+end;
+$$;
+
+select count_appear (20, TIMESTAMP '2023-06-07 10:40:28', TIMESTAMP '2023-06-07 20:40:28');
+
+-- cau 4
+
+create or replace function range_movement (x int, y int, r float, start_timestamp timestamp, end_timestamp timestamp)
+returns table (name varchar(500), age int, lat int, lon int, distance float, miliseconds bigint)
+language plpgsql
+as $$
+declare
+start_miliseconds bigint;
+end_miliseconds bigint;
+begin
+	SELECT EXTRACT(EPOCH FROM  start_timestamp) * 1000 into start_miliseconds;
+	SELECT EXTRACT(EPOCH FROM  end_timestamp )* 1000 into end_miliseconds;
+	
+	return query 
+		select * from 
+		(select b."name" , b.age , a.lat , a.lon, distance(a.lat, a.lon, x, y) distance, a.millisecond 
+		from tracking a inner join users b on a.user_id = b.user_id 
+		where a.millisecond < end_miliseconds and a.millisecond>= start_miliseconds) c
+		where c.distance <= r;
+end;
+$$ ;
+
+select * from range_movement (0, 0, 100, TIMESTAMP '2023-06-07 10:40:28', TIMESTAMP '2023-06-07 20:40:28');
+
+-- cau 5
+
+create or replace function sum_movement (userId int, start_timestamp timestamp, end_timestamp timestamp)
+returns float
+language plpgsql
+as $$
+declare 
+start_miliseconds bigint;
+end_miliseconds bigint;
+sum_distance float;
+begin
+	SELECT EXTRACT(EPOCH FROM  start_timestamp) * 1000 into start_miliseconds;
+	SELECT EXTRACT(EPOCH FROM  end_timestamp )* 1000 into end_miliseconds;
+
+	
+	select sum(distance(lat, lat1, lon, lon1))
+	from 
+	(
+		select lat, lon, (lead(lat) over (order by millisecond)) lat1, (lead(lon) over (order by millisecond)) lon1
+		from tracking t 
+		where user_id = userId and t.millisecond >= start_miliseconds and t.millisecond  < end_miliseconds
+		order by t.millisecond 
+	) t1
+	where t1.lat1 is not null into sum_distance;
+
+	if(sum_distance is null) then
+		return 0;
+	end if;
+	return sum_distance;
+	
+end;
+$$;
+
+select sum_movement (10, TIMESTAMP '2023-06-07 10:40:28', TIMESTAMP '2023-06-07 20:40:28');
+
+-- cau 6
+
+create or replace function quality_movement (userId int, start_timestamp timestamp, end_timestamp timestamp)
+returns varchar(20)
+language plpgsql
+as $$
+declare 
+sum_distance float;
+begin	
+	select sum_movement(userId, start_timestamp, end_timestamp) into sum_distance;
+	if(sum_distance > 1000) then return 'long distance';
+	else return 'close distance'; end if;
+end;
+$$;
+
+select quality_movement(10, TIMESTAMP '2023-06-07 10:40:28', TIMESTAMP '2023-06-07 20:40:28');
+
+
+
+
